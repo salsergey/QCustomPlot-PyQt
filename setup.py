325b08ee4cee5aa39369633d07c25648078d8526
Sergey Salnikov <salsergey@gmail.com>
2019-09-07 11:36:00 +0700

    Improved support for different platforms and Python versions
    * Refactored setup.py to support both Python 2 and Python 3.
    * Added support for building from another directories.
    * Improved automatic determination of qmake.exe if it is in PATH.
    * Prepared for PyPI.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 import os
 import platform
 import subprocess
+import sys
 
 import sipconfig
 
@@ -18,6 +19,8 @@ from sipdistutils import build_ext
 from PyQt5.QtCore import PYQT_CONFIGURATION
 from PyQt5.QtCore import QLibraryInfo
 
+CPU_COUNT = os.cpu_count() if 'cpu_count' in dir(os) else 1 # number of parallel compilations
+
 # monkey-patch for parallel compilation, see
 # https://stackoverflow.com/questions/11013851/speeding-up-build-process-with-distutils
 def parallelCCompile(self, sources, output_dir=None, macros=None, include_dirs=None, debug=0, extra_preargs=None, extra_postargs=None, depends=None):
@@ -25,14 +28,13 @@ def parallelCCompile(self, sources, output_dir=None, macros=None, include_dirs=N
     macros, objects, extra_postargs, pp_opts, build = self._setup_compile(output_dir, macros, include_dirs, sources, depends, extra_postargs)
     cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
     # parallel code
-    N = os.cpu_count() # number of parallel compilations
     import multiprocessing.pool
     def _single_compile(obj):
         try: src, ext = build[obj]
         except KeyError: return
         self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
     # convert to list, imap is evaluated on-demand
-    list(multiprocessing.pool.ThreadPool(N).imap(_single_compile,objects))
+    list(multiprocessing.pool.ThreadPool(CPU_COUNT).imap(_single_compile,objects))
     return objects
 
 
@@ -42,19 +44,29 @@ LINUX_HOST = (platform.system() == 'Linux')
 # This is with Unix pathsep even on windows
 QT_BINARIES = QLibraryInfo.location(QLibraryInfo.BinariesPath)
 if WINDOWS_HOST:
-    # Default to MSVC nmake
+    # Default to Qt's jom instead of MSVC nmake
     DEFAULT_MAKE = 'jom.exe'
-    DEFAULT_QMAKE = "{}/{}".format(QT_BINARIES, "qmake.exe")
+    # Try to find qmake.exe in PATH
+    pipe = subprocess.Popen('where qmake.exe', stdout=subprocess.PIPE)
+    (stdout, stderr) = pipe.communicate()
+    DEFAULT_QMAKE = str(stdout.strip(), 'utf8') if sys.version_info.major == 3 else str(stdout.strip())
+    if DEFAULT_QMAKE == '':
+        print('Unable to find qmake.exe. Please set the path manually using --qmake option.')
 else:
     DEFAULT_MAKE = 'make'
-    DEFAULT_QMAKE = "{}/{}".format(QT_BINARIES, "qmake")
+    DEFAULT_QMAKE = '{}/{}'.format(QT_BINARIES, 'qmake')
 
 DEFAULT_QT_INCLUDE = QLibraryInfo.location(QLibraryInfo.HeadersPath)
 ROOT = abspath(dirname(__file__))
+BUILD_DIR = os.getcwd()
 BUILD_STATIC_DIR = join(ROOT, 'lib-static')
 
 # Monkey-patch, see above
 CCompiler.compile=parallelCCompile
+
+
+with open(join(ROOT, 'README.md'), 'r') as fh:
+    long_description = fh.read()
 
 
 class MyBuilderExt(build_ext):
@@ -88,15 +100,15 @@ class MyBuilderExt(build_ext):
             print('Setting make to \'%s\'' % DEFAULT_MAKE)
             self.make = DEFAULT_MAKE
         if self.qt_include_dir is None:
-            pipe = subprocess.Popen([self.qmake, "-query", "QT_INSTALL_HEADERS"], stdout=subprocess.PIPE)
+            pipe = subprocess.Popen([self.qmake, '-query', 'QT_INSTALL_HEADERS'], stdout=subprocess.PIPE)
             (stdout, stderr) = pipe.communicate()
-            self.qt_include_dir = str(stdout.strip(), 'utf8')
+            self.qt_include_dir = str(stdout.strip(), 'utf8') if sys.version_info.major == 3 else str(stdout.strip())
             print('Setting Qt include dir to \'%s\'' % self.qt_include_dir)
 
         if self.qt_library_dir is None:
-            pipe = subprocess.Popen([self.qmake, "-query", "QT_INSTALL_LIBS"], stdout=subprocess.PIPE)
+            pipe = subprocess.Popen([self.qmake, '-query', 'QT_INSTALL_LIBS'], stdout=subprocess.PIPE)
             (stdout, stderr) = pipe.communicate()
-            self.qt_library_dir = str(stdout.strip(), 'utf8')
+            self.qt_library_dir = str(stdout.strip(), 'utf8') if sys.version_info.major == 3 else str(stdout.strip())
             print('Setting Qt library dir to \'%s\'' % self.qt_library_dir)
 
         if not exists(self.qmake):
@@ -104,24 +116,27 @@ class MyBuilderExt(build_ext):
 
     def __build_qcustomplot_library(self):
         if WINDOWS_HOST:
-            qcustomplot_static = join(self.build_temp, 'release', 'qcustomplot.lib')
+            qcustomplot_static = join(BUILD_DIR, self.build_temp, 'release', 'qcustomplot.lib')
         else:
-            qcustomplot_static = join(self.build_temp, 'libqcustomplot.a')
+            qcustomplot_static = join(BUILD_DIR, self.build_temp, 'libqcustomplot.a')
         if exists(qcustomplot_static):
             return
 
-        os.makedirs(self.build_temp, exist_ok=True)
+        if sys.version_info.major == 3:
+          os.makedirs(self.build_temp, exist_ok=True)
+        else:
+          os.makedirs(self.build_temp)
         os.chdir(self.build_temp)
         print('Make static qcustomplot library...')
         self.spawn([self.qmake, join(ROOT, 'QCustomPlot/src/qcp-staticlib.pro')])
         # AFAIK only nmake does not support -j option
-        has_multiprocess = not(WINDOWS_HOST and "nmake"in self.make)
+        has_multiprocess = not(WINDOWS_HOST and 'nmake' in self.make)
         make_cmdline = [self.make]
         if has_multiprocess:
-            make_cmdline.extend(['-j', str(os.cpu_count())])
-        self.spawn([self.make, '-j', str(os.cpu_count())])
+            make_cmdline.extend(['-j', str(CPU_COUNT)])
+        self.spawn([self.make, '-j', str(CPU_COUNT)])
 
-        os.chdir(ROOT)
+        os.chdir(BUILD_DIR)
         self.static_lib = qcustomplot_static
         # Possibly it's hack
         qcustomplot_ext = self.extensions[0]
@@ -141,6 +156,7 @@ class MyBuilderExt(build_ext):
             for subdir in ['.', 'QtCore', 'QtGui', 'QtWidgets', 'QtPrintSupport']
         ]
         qcustomplot_ext.library_dirs += [
+            BUILD_DIR,
             self.build_temp,
             self.qt_library_dir
         ]
@@ -167,21 +183,41 @@ setup(
     name='QCustomPlot2',
     version='2.0.1',
     description='QCustomPlot is a Qt widget for plotting and data visualization',
-    author='Dmitry Voronin, Giuseppe Corbelli, Christopher Gilbert',
-    author_email='carriingfate92@yandex.ru',
-    url='https://github.com/cjgdev/QCustomPlot2-PyQt5',
-    platforms=['Linux'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Dmitry Voronin, Giuseppe Corbelli, Christopher Gilbert, Sergey Salnikov',
+    author_email='salsergey@gmail.com',
+    url='https://osdn.net/users/salsergey/pf/QCustomPlot2-PyQt5',
+    platforms=['Linux', 'Windows'],
     license='MIT',
-    ext_modules=[
-        Extension(
-            'QCustomPlot2',
-            ['sip/all.sip'],
-            include_dirs=['.','./sip']
-        ),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: User Interfaces'
     ],
     requires=[
         'sipconfig',
         'PyQt5'
+    ],
+    install_requires=['PyQt5'],
+    ext_modules=[
+        Extension(
+            'QCustomPlot2',
+            [join(ROOT, 'sip/all.sip')],
+            include_dirs=[ROOT, join(ROOT, 'sip')]
+        ),
     ],
     cmdclass={'build_ext': MyBuilderExt}
 )
