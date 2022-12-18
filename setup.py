128cb4395b5053d529894c2964d834178b1a5b20
Sergey Salnikov <salsergey@gmail.com>
2022-12-18 11:14:12 +0700

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -19,22 +19,33 @@ from sipdistutils import build_ext
 from PyQt5.QtCore import PYQT_CONFIGURATION
 from PyQt5.QtCore import QLibraryInfo
 
-CPU_COUNT = os.cpu_count() if 'cpu_count' in dir(os) else 1 # number of parallel compilations
+
+# Number of parallel compilations
+CPU_COUNT = os.cpu_count() if 'cpu_count' in dir(os) else 1
+
 
 # monkey-patch for parallel compilation, see
 # https://stackoverflow.com/questions/11013851/speeding-up-build-process-with-distutils
-def parallelCCompile(self, sources, output_dir=None, macros=None, include_dirs=None, debug=0, extra_preargs=None, extra_postargs=None, depends=None):
+def parallelCCompile(self, sources, output_dir=None, macros=None,
+                     include_dirs=None, debug=0, extra_preargs=None,
+                     extra_postargs=None, depends=None):
     # those lines are copied from distutils.ccompiler.CCompiler directly
-    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(output_dir, macros, include_dirs, sources, depends, extra_postargs)
+    macros, objects, extra_postargs, pp_opts, build = \
+        self._setup_compile(output_dir, macros, include_dirs,
+                            sources, depends, extra_postargs)
     cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
     # parallel code
     import multiprocessing.pool
+
     def _single_compile(obj):
-        try: src, ext = build[obj]
-        except KeyError: return
+        try:
+            src, ext = build[obj]
+        except KeyError:
+            return
         self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+
     # convert to list, imap is evaluated on-demand
-    list(multiprocessing.pool.ThreadPool(CPU_COUNT).imap(_single_compile,objects))
+    list(multiprocessing.pool.ThreadPool(CPU_COUNT).imap(_single_compile, objects))
     return objects
 
 
@@ -51,7 +62,8 @@ if WINDOWS_HOST:
     (stdout, stderr) = pipe.communicate()
     DEFAULT_QMAKE = str(stdout.strip(), 'utf8') if sys.version_info.major == 3 else str(stdout.strip())
     if DEFAULT_QMAKE == '':
-        print('Unable to find qmake.exe. Please set the path manually using --qmake option.')
+        print('Unable to find qmake.exe.'
+              'Please set the path manually using --qmake option.')
 else:
     DEFAULT_MAKE = 'make'
     DEFAULT_QMAKE = '{}/{}'.format(QT_BINARIES, 'qmake')
@@ -62,7 +74,7 @@ BUILD_DIR = os.getcwd()
 BUILD_STATIC_DIR = join(ROOT, 'lib-static')
 
 # Monkey-patch, see above
-CCompiler.compile=parallelCCompile
+CCompiler.compile = parallelCCompile
 
 
 with open(join(ROOT, 'README.md'), 'r') as fh:
@@ -100,13 +112,15 @@ class MyBuilderExt(build_ext):
             print('Setting make to \'%s\'' % DEFAULT_MAKE)
             self.make = DEFAULT_MAKE
         if self.qt_include_dir is None:
-            pipe = subprocess.Popen([self.qmake, '-query', 'QT_INSTALL_HEADERS'], stdout=subprocess.PIPE)
+            pipe = subprocess.Popen([self.qmake, '-query', 'QT_INSTALL_HEADERS'],
+                                    stdout=subprocess.PIPE)
             (stdout, stderr) = pipe.communicate()
             self.qt_include_dir = str(stdout.strip(), 'utf8') if sys.version_info.major == 3 else str(stdout.strip())
             print('Setting Qt include dir to \'%s\'' % self.qt_include_dir)
 
         if self.qt_library_dir is None:
-            pipe = subprocess.Popen([self.qmake, '-query', 'QT_INSTALL_LIBS'], stdout=subprocess.PIPE)
+            pipe = subprocess.Popen([self.qmake, '-query', 'QT_INSTALL_LIBS'],
+                                    stdout=subprocess.PIPE)
             (stdout, stderr) = pipe.communicate()
             self.qt_library_dir = str(stdout.strip(), 'utf8') if sys.version_info.major == 3 else str(stdout.strip())
             print('Setting Qt library dir to \'%s\'' % self.qt_library_dir)
@@ -116,22 +130,23 @@ class MyBuilderExt(build_ext):
 
     def __build_qcustomplot_library(self):
         if WINDOWS_HOST:
-            qcustomplot_static = join(BUILD_DIR, self.build_temp, 'release', 'qcustomplot.lib')
+            qcustomplot_static = join(BUILD_DIR, self.build_temp,
+                                      'release', 'qcustomplot.lib')
         else:
             qcustomplot_static = join(BUILD_DIR, self.build_temp, 'libqcustomplot.a')
         if exists(qcustomplot_static):
             return
 
         if sys.version_info.major == 3:
-          os.makedirs(self.build_temp, exist_ok=True)
+            os.makedirs(self.build_temp, exist_ok=True)
         else:
-          os.makedirs(self.build_temp)
+            os.makedirs(self.build_temp)
         os.chdir(self.build_temp)
         print('\nMaking static qcustomplot library...\n')
-        self.spawn([self.qmake, join(ROOT, 'src/qcp-staticlib.pro'), 'DESTDIR='])
+        self.spawn([self.qmake, join(ROOT, 'src/qcp-staticlib.pro')])
         # AFAIK only nmake does not support -j option
-        has_multiprocess = not(WINDOWS_HOST and 'nmake' in self.make)
-        make_cmdline = [self.make, 'DESTDIR=']
+        has_multiprocess = not (WINDOWS_HOST and 'nmake' in self.make)
+        make_cmdline = [self.make]
         if has_multiprocess:
             make_cmdline.extend(['-j', str(CPU_COUNT)])
         self.spawn(make_cmdline)
@@ -179,15 +194,16 @@ class MyBuilderExt(build_ext):
         cfg = sipconfig.Configuration()
         return join(cfg.default_sip_dir, 'PyQt5')
 
+
 setup(
-    name='QCustomPlot2',
+    name='QCustomPlot_PyQt5',
     version='2.1.1',
     description='QCustomPlot is a Qt widget for plotting and data visualization',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Dmitry Voronin, Giuseppe Corbelli, Christopher Gilbert, Sergey Salnikov',
     author_email='salsergey@gmail.com',
-    url='https://github.com/salsergey/QCustomPlot2-PyQt5',
+    url='https://github.com/salsergey/QCustomPlot-PyQt',
     platforms=['Linux', 'Windows'],
     license='MIT',
     classifiers=[
@@ -217,8 +233,8 @@ setup(
     ],
     ext_modules=[
         Extension(
-            'QCustomPlot2',
-            [join(ROOT, 'sip/all.sip')],
+            'QCustomPlot_PyQt5',
+            [join(ROOT, 'sip/all_PyQt5.sip')],
             include_dirs=[join(ROOT, 'src'), join(ROOT, 'sip')]
         ),
     ],
